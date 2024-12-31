9b4d04c81863fd4611fe2a95953758d9de5a4e8e
Kaique Rocha <kaique@MacBook-Pro-de-Kaique.local>
2024-12-31 08:15:24 -0300

    feat: fazendo pooling manualmente

--- src/main/java/br/com/kaiquera/DemoPropertiesWatcher/watcher/ConfigWatcher.java
+++ src/main/java/br/com/kaiquera/DemoPropertiesWatcher/watcher/ConfigWatcher.java
@@ -5,13 +5,18 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.FileTime;
+import java.time.Duration;
 import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
+@EnableScheduling
 public class ConfigWatcher {
     private final ConfigurableEnvironment environment;
     private static final String CONFIG_FILE_OUTDOOR = "./config/conf.properties";
@@ -19,39 +24,65 @@ public class ConfigWatcher {
     private static final String PROPERTY_SOURCE_NAME = "dynamicProperties";
     private volatile FileTime currentLastModifiedTime = FileTime.fromMillis(0);
 
-    public ConfigWatcher(ConfigurableEnvironment environment) {
+    public ConfigWatcher(ConfigurableEnvironment environment) throws IOException {
         this.environment = environment;
-        initializeWatcher();
+        addInitialPropertySource();
+        startPolling();
     }
 
-    private void initializeWatcher() {
-        try {
-            Path path = Paths.get(CONFIG_FILE_OUTDOOR).getParent();
-            WatchService watchService = FileSystems.getDefault().newWatchService();
-            path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+//    private void initializeWatcher() {
+//        try {
+//            Path path = Paths.get(CONFIG_FILE_OUTDOOR).getParent();
+//            WatchService watchService = FileSystems.getDefault().newWatchService();
+//            path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+//
+//            addInitialPropertySource();
+//
+//            Thread.startVirtualThread(() -> {
+//                while (true) {
+//                    try {
+//                        WatchKey key = watchService.take();
+//                        for (WatchEvent<?> event : key.pollEvents()) {
+//                                reloadProperties();
+//                        }
+//                        key.reset();
+//                    } catch (InterruptedException e) {
+//                        Thread.currentThread().interrupt();
+//                        return;
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-            addInitialPropertySource();
+    public void startPolling() {
+        Thread.startVirtualThread(() -> {
+            while (true) {
+                try {
+                    Path path = Paths.get(CONFIG_FILE_OUTDOOR);
 
-            Thread.startVirtualThread(() -> {
-                while (true) {
-                    try {
-                        WatchKey key = watchService.take();
-                        for (WatchEvent<?> event : key.pollEvents()) {
-                                reloadProperties();
-                        }
-                        key.reset();
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        return;
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    // Obtém o tempo da última modificação
+                    FileTime lastModifiedTime = Files.getLastModifiedTime(path);
+
+                    // Compara com o valor armazenado
+                    if (!lastModifiedTime.equals(currentLastModifiedTime)) {
+                        currentLastModifiedTime = lastModifiedTime; // Atualiza o tempo
+                        reloadProperties(); // Recarrega as propriedades
                     }
+                    Thread.sleep(Duration.ofSeconds(10L));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
-            });
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            }
+        });
     }
 
     private void addInitialPropertySource() throws IOException {
